5202d084dddb267172a07c777f709460c4ed9d16
Sean M. Collins <sean@coreitpro.com>
2021-04-02 13:42:00 -0400

    Add java classes from step 4

---
+++ projects/batfish/src/main/java/org/batfish/grammar/dellos10/Dellos10CombinedParser.java
@@ -0,0 +1,8 @@
+package org.batfish.grammar.dellos10;
+
+import org.batfish.grammar.BatfishCombinedParser;
+
+
+public class Dellos10CombinedParser extends BatfishCombinedParser<DellosParser, DellosLexer> {
+
+}
